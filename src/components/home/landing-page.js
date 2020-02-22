476a412b295b1f62726e027f8ca25c0773b8d3df
Rob P <robpeterman573@gmail.com>
2020-02-21 21:09:00 -0500

    Added basic styling + bg video for landing page

--- src/components/home/landing-page.js
+++ src/components/home/landing-page.js
@@ -1,11 +1,16 @@
 import React from "react";
 import SearchInput from "./search-input";
+import bgVideo from "../../media/landing-bg.mp4";
 
 export default class LandingPage extends React.Component {
   render() {
     return (
       <div className="landing-page">
-        <div className='landing-page__search-wrapper'>
+        <video className="landing-page__bg-video"  width="100%" autoPlay loop muted>
+          <source src={bgVideo} type="video/mp4" />
+        </video>
+        <div className="landing-page__search-wrapper">
+          <h1>Countries of the World</h1>
           <SearchInput />
         </div>
       </div>
